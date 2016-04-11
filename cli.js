8aa6d896595b641cf010b77327e3123bef194dc2
Welkson Ramos <welksonr@hotmail.com>
2016-04-11 12:32:35 -0300

    improve ES6

--- cli.js
+++ cli.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 'use strict';
-var meow = require('meow');
-var isPublicDomain = require('is-public-domain');
 
-var cli = meow({
-	help:[
-	'Usage',
-	'$ domain <url>',
-	'Example',
-	'$ domain sindresorhus.com',
-	]
-});
+const meow = require('meow');
+const isPublicDomain = require('is-public-domain');
 
-var input = cli.input[0];
+const cli = meow(`
+	Usage
+	$ domain <url>
 
-if(!input){
+	Example
+	$ domain sindresorhus.com
+	`);
+
+const input = cli.input[0];
+
+if(!input) {
 	console.error('Expecify a url');
 	process.exit(1);
 }
